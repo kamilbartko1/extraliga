b79bc8b26ed88d4903777dc020cf9be62cdd9f0c
kamilbartko1 <kamil.bartko1@gmail.com>
2026-01-05 20:42:10 +0100

    Fixed final repositioning call to use shared function

--- public/app.js
+++ public/app.js
@@ -3892,9 +3892,11 @@ async function showVipTipAnalysis(playerName, teamCode, oppCode, event) {
     <button class="close-modal-btn" onclick="closeVipTipAnalysis()">${t("common.close")}</button>
   `;
   
-  // Uprav pozíciu po načítaní obsahu (výška modalu sa mohla zmeniť)
+  // Reposition after content loads (height may have changed)
   requestAnimationFrame(() => {
-    requestAnimationFrame(setModalPosition);
+    requestAnimationFrame(() => {
+      positionModalInViewport(modalContent, btnRect);
+    });
   });
 }
 
